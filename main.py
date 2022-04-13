5ec0b44bdd9bad2d2fef82d2a468e52559a2da77
Near <singleangel@vip.qq.com>
2022-04-13 11:43:51 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from multiprocessing import Pool
 
 if __name__ == '__main__':
     # keywords = ['cyber physical', 'digital twin', 'meta']
-    keywords = ['cyber physical']
+    keywords = ['data']
     venue_names = ['JSAC', 
                     'TMC', 
                     'TON', 
@@ -14,16 +14,17 @@ if __name__ == '__main__':
                     'NSDI']
     # venue_names = ['Conference on Applications, Technologies, Architectures, and Protocols for Computer Communication', 
     #                 'MobiCom', 'IEEE Conference on Computer Communications', 'NSDI']
-    query_number = 1000
-    maximum_query_number = 50
-    # pool = Pool(processes=4)
-    # for venue_name in venue_names:
-    #     pool.apply_async(dblp.search_by_keywords_venues, (keywords, [venue_name], query_number, maximum_query_number))
-    # print('Waiting for all subprocesses done...')
-    # pool.close()
-    # pool.join()
-    # print('All subprocesses done.')
-
     query_number = 5000
     maximum_query_number = 50
-    dblp.search_by_keywords(keywords, query_number, maximum_query_number)
+    
+    pool = Pool(processes=5)
+    for venue_name in venue_names:
+        pool.apply_async(dblp.search_by_keywords_venues, (keywords, [venue_name], query_number, maximum_query_number))
+    print('Waiting for all subprocesses done...')
+    pool.close()
+    pool.join()
+    print('All subprocesses done.')
+
+    # query_number = 5000
+    # maximum_query_number = 50
+    # dblp.search_by_keywords(keywords, query_number, maximum_query_number)
